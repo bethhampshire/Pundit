9fae2cc4377ad868186d7360ea778eb4e1b81226
bethhampshire <beth-hampshire94@hotmail.co.uk>
2022-10-24 23:08:11 +0100

    first day

---
+++ InitialDataSetUp/TypeOneData/TypeOneDataExtractor.cs
@@ -0,0 +1,65 @@
+﻿using InitialDataSetUp.TypeOneData;
+using System.IO.Compression;
+using System.Text;
+
+namespace InitialDataSetUp
+{
+    public class TypeOneDataExtractor
+    {
+        public List<TypeOneDataModel> GetData()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo("C:/Projects/pundit/Data/typeOneData");
+            FileInfo[] ZipFolders = directoryInfo.GetFiles();
+
+            List<TypeOneDataModel> typeOneDataModels = new List<TypeOneDataModel>();
+
+            foreach (FileInfo ZipFolder in ZipFolders)
+            {
+                TypeOneDataModel typeOneDataModel = new TypeOneDataModel();
+
+                string[] splitFolderName = ZipFolder.Name.Split('-');
+
+                typeOneDataModel.Nationality = splitFolderName[0];
+
+                typeOneDataModel.LeagueName = GetLeagueName(splitFolderName);
+
+                typeOneDataModel.Clubs = GetClubNames(ZipFolder.Name, directoryInfo);
+
+                typeOneDataModels.Add(typeOneDataModel);
+            }
+
+            return typeOneDataModels;
+        }
+
+        private List<string> GetClubNames(string ZipFolderName, DirectoryInfo directoryInfo)
+        {
+            List<string> ClubNames = new List<string>();
+
+            string zipFilePath = ZipFolderName;
+            string extractionPath = directoryInfo.FullName;
+            Console.WriteLine(zipFilePath);
+            Console.WriteLine(extractionPath);
+
+            return ClubNames;
+        }
+
+        private string GetLeagueName(string[] splitFolderName)
+        {
+            var leagueName = new StringBuilder();
+
+            foreach (string splitFolder in splitFolderName)
+            {
+                if (splitFolder == splitFolderName.Last())
+                {
+                    string[] finalSplit = splitFolder.Split('_');
+                    leagueName.Append(finalSplit[0]);
+                } 
+                else if (splitFolder != splitFolderName.First())
+                {
+                    leagueName.Append(splitFolder + " ");
+                }
+            }
+            return leagueName.ToString();
+        }
+    }
+}
---
+++ InitialDataSetUp/TypeOneData/TypeOneDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InitialDataSetUp.TypeOneData
+{
+    public class TypeOneDataModel
+    {
+        public string Nationality { get; set; }
+        public string LeagueName { get; set; }
+        public List<string> Clubs { get; set; }
+    }
+}
